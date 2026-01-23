2806d8c0305a60555fc20835ace3b41e0eeafd22
Claudiu Persoiu <claudiu@claudiupersoiu.ro>
2026-01-23 09:16:32 +0200

    added restart functionality

--- wasm/app.go
+++ wasm/app.go
@@ -12,8 +12,7 @@ func main() {
 
 	c := make(chan struct{})
 
-	window := js.Global().Get("window")
-	window.Set("onload", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	restartFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		level := internal.GetLevelValues()
 
 		if level.X > 0 && level.Y > 0 && level.Bombs > 0 {
@@ -21,7 +20,13 @@ func main() {
 		}
 
 		return nil
-	}))
+	})
+
+	window := js.Global().Get("window")
+	window.Set("onload", restartFunc)
+
+	document := js.Global().Get("document")
+	document.Call("getElementById", "new-game").Set("onclick", restartFunc)
 
 	// https://egghead.io/lessons/go-call-a-go-webassembly-function-from-javascript
 
